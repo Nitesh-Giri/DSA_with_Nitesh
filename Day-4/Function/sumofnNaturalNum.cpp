14bc7ca1ac11e73a9eab0ac8b811ed9a84ed89d6
Nitesh Giri <niteshgiri068@gmail.com>
2024-09-17 07:39:10 +0530

    updated

--- Day-4/Function/sumofnNaturalNum.cpp
+++ Day-4/Function/sumofnNaturalNum.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int sum(int n){
-    return n*(n+1)/2;
+    //method 1 recursively
+
+    // return n*(n+1)/2;
+
+    //method 2
+
+    int sum =0;
+    for(int i=1; i<=n; i++){
+        sum+=i;
+    }
+    return sum;
 }
 
 int main(){
