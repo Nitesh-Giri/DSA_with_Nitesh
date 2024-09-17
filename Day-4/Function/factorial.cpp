4a976902c2e23b22491598cde223299981aeec98
Nitesh Giri <niteshgiri068@gmail.com>
2024-09-17 07:38:02 +0530

    add

--- Day-4/Function/factorial.cpp
+++ Day-4/Function/factorial.cpp
@@ -5,9 +5,17 @@ int fact(int n){
 
     //method 1;
 
-    if(n==0 || n==1)
-        return 1;
-    return n*fact(n-1);
+    // if(n==0 || n==1)
+    //     return 1;
+    // return n*fact(n-1);
+
+    //metho 2
+
+    int fact =1;
+    for(int i=1; i<=n; i++){
+        fact = fact*i;
+    }
+    return fact;
 }
 int main(){
 
