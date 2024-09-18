9f084f07823813e6e6b0603ff9ce1942d4e2f743
Nitesh Giri <niteshgiri068@gmail.com>
2024-09-19 00:25:49 +0530

    updated

--- Day-4/Function/calSumOfDigit.cpp
+++ Day-4/Function/calSumOfDigit.cpp
@@ -3,11 +3,19 @@ using namespace std;
 
 int sumOfDigit(int n){
 
-    
+    //method 1 recursively
+    // if(n==0)
+    //     return 0;
+    // return (n%10) + sumOfDigit(n/10);
 
-    if(n==0)
-        return 0;
-    return (n%10) + sumOfDigit(n/10);
+    //method 2
+    int sum =0;
+    while(n>0){
+        sum+=n%10;
+        n=n/10;
+    }
+
+    return sum;
 }
 
 int main(){
