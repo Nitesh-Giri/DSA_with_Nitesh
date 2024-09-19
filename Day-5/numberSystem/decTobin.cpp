c202195e584c5d0996501cb34ed7a9e523c8d9a3
Nitesh Giri <niteshgiri068@gmail.com>
2024-09-19 18:11:33 +0530

    update

--- Day-5/numberSystem/decTobin.cpp
+++ Day-5/numberSystem/decTobin.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
 using namespace std;
+
+int decTobin(int n){
+    int ans =0;
+    int pow =1;
+
+    while(n>0){
+        int rem = n%2;
+        ans = ans + rem*pow;
+        pow = pow*10;
+        n = n/2;
+    };
+    return ans;
+}
 int main(){
+
     int n;
+    cout<<"Enter a number: ";
     cin>>n;
-    int ans=0;
-    int i=0;
-    while(n!=0){
-        int bit=n&1;
-        ans=(bit*pow(10,i))+ans;
-        n=n>>1;
-        i++;
-    }
-    cout<<ans<<endl;
+
+    cout<<decTobin(n)<<endl;
     return 0;
 }
