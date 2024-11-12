8f091770bfd654257fb9b65e4cfae997d4d0e254
Nitesh Giri <niteshgiri068@gmail.com>
2024-11-12 21:18:17 +0530

    add

---
+++ Pointer/passByRef.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void changeA(int &b){ // pass by alias
+    b=20;
+}
+int main()
+{
+    int a=10;
+    changeA(a);
+    cout<<"Inside function a: "<<a<<endl;
+    
+    return 0;
+}
